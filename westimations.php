def4c724171aa987f8f90dd2838cdf3c15b58059
Puleeno Nguyen <puleeno@gmail.com>
2020-11-13 11:52:31 +0700

    Added plugin files

---
+++ westimations.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Westimations
+ * Plugin URI: https://github.com/ramphor/westimations
+ * Author: Ramphor Premium
+ * Author URI: https://puleeno.com
+ * Version: 1.0.0
+ * Description: Make estimations for Woocommerce product
+ */
